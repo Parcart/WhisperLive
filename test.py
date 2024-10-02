be848ad9941e5e9199655fe0ffcee0815663f531
Parfenov Maksim <makcc2992@gmail.com>
2024-10-02 18:49:15 +0500

    my test file

--- test.py
+++ test.py
@@ -1,19 +1,49 @@
 import asyncio
 import multiprocessing
 import time
+import wave
+# import aiofiles
+import aiofiles
+import pyaudio
 
-from whisper_live.client import TranscriptionClient
+from whisper_live.async_client import AsyncTranscriptionClient as TranscriptionClient
 import whisper_live.utils as utils
 
-client = TranscriptionClient(
-    "localhost",
-    9090,
-    lang=None,
-    translate=False,
-    model="large-v2",
-    use_vad=False
-)
+# client = TranscriptionClient(
+#     "localhost",
+#     9090,
+#     lang=None,
+#     translate=False,
+#     model="large-v2",
+#     use_vad=False
+# )
+async def async_audio_stream_generator(filename):
+    chunk = 4096
+    try:
+        async with aiofiles.open(filename, "rb") as wavfile:
+            start_time = time.time()
+        # with wave.open(filename, "rb") as wavfile:
+            try:
+                while True:
+                    # print("INFO: streaming file", filename)
+                    data = await wavfile.read(chunk)
+                    if data == b"":
+                        end_time = time.time()
+                        elapsed_time = end_time - start_time
+                        print(f"INFO Время чтения файла {filename}: {elapsed_time:.4f} секунд")
+                        break
 
+                    await asyncio.sleep(0.11)
+
+                    yield data
+            except Exception as e:
+                print(e)
+                pass
+            finally:
+                print("INFO: Closing file", filename)
+                await wavfile.close()
+    except Exception as e:
+        print(e)
 
 # client()
 
@@ -26,39 +56,62 @@ async def readfile(filename):
             yield data
 
 
-# async def omg():
-#     async for data in readfile("tests/test_woman"):
-#         print(data)
-#
-# asyncio.run(omg())
-
-
-
-
-# asyncio.run(client(file_async_generator=readfile(resampled_file)))
-def send_file(command):
+async def async_FileStreamSTT(client, filename, name):
     start_time = time.time()
-    current_loop = asyncio.get_event_loop()
-    resampled_file = utils.resample("tests/test_1.ogg")
-    client(pcm_generator=readfile(resampled_file), event_loop=current_loop)
+    await client(filename)
+    result = "".join([transcript["text"] for transcript in client.clients[0].transcript])
     end_time = time.time()
     elapsed_time = end_time - start_time
-    print(f"Время выполнения команды '{command}': {elapsed_time:.4f} секунд")
+    print(f"Время выполнения команды '{name}': {elapsed_time:.4f} секунд")
+    print("Результат выполнения STT: " + result)
 
-
-def stream_file(command):
+async def async_AudioStreamSTT(client, filename, name):
     start_time = time.time()
-    client("tests/test_1.ogg")
+    request_iterator = async_audio_stream_generator(filename)
+    await client(async_audio_generator=request_iterator)
+    result = "".join([transcript["text"] for transcript in client.clients[0].transcript])
     end_time = time.time()
     elapsed_time = end_time - start_time
-    print(f"Время выполнения команды '{command}': {elapsed_time:.4f} секунд")
+    print(f"Время выполнения команды '{name}': {elapsed_time:.4f} секунд")
+    print("Результат выполнения STT: " + result)
 
 
-stream_file("1")
+async def create_and_run_tasks():
+    tasks = list()
+    client = TranscriptionClient(
+        "localhost",
+        9090,
+        lang=None,
+        translate=False,
+        model="large-v2",
+        use_vad=False,
+        eventloop=asyncio.get_running_loop()
+    )
+    tasks.append(asyncio.create_task(async_AudioStreamSTT(client, "tests/test_woman_resampled16000.wav", "Test1"), name="Test1"))
+    client2 = TranscriptionClient(
+        "localhost",
+        9090,
+        lang=None,
+        translate=False,
+        model="large-v2",
+        use_vad=False,
+        eventloop=asyncio.get_running_loop()
+    )
+    tasks.append(asyncio.create_task(async_AudioStreamSTT(client2, "tests/test_1_resampled.wav", "Test2"), name="Test2"))
+    result = asyncio.gather(*tasks)
+    await result
 
-# send_file("2")
 
-# if __name__ == '__main__':
+async def main():
+
+    await create_and_run_tasks()
+    pass
+
+
+if __name__ == '__main__':
+    # with open("tests/test_woman_resampled41000.wav", "rb") as f:
+    #     data = f.read()
+    asyncio.run(main())
+
 #     with multiprocessing.Pool(processes=4) as pool:
 #         pool.map(stream_file, ["1", "2", "3", "4"])
-
