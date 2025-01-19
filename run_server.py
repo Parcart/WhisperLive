04e5e9ed7b8ff74745eb0cbad6eca260053cfcfd
Parfenov Maksim <makcc2992@gmail.com>
2025-01-19 17:29:55 +0500

    fix libs

--- run_server.py
+++ run_server.py
@@ -1,85 +1,1 @@
-import argparse
-import os
-
-from faster_whisper import download_model
-
-from whisper_live.support_rabbitmq import RabbitMQConsumer
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--port', '-p',
-                        type=int,
-                        default=9090,
-                        help="Websocket port to run the server on.")
-    parser.add_argument('--backend', '-b',
-                        type=str,
-                        default='faster_whisper',
-                        help='Backends from ["tensorrt", "faster_whisper"]')
-    parser.add_argument('--faster_whisper_custom_model_path', '-fw',
-                        type=str, default=None,
-                        help="Custom Faster Whisper Model")
-    parser.add_argument('--trt_model_path', '-trt',
-                        type=str,
-                        default=None,
-                        help='Whisper TensorRT model path')
-    parser.add_argument('--trt_multilingual', '-m',
-                        action="store_true",
-                        help='Boolean only for TensorRT model. True if multilingual.')
-    parser.add_argument('--omp_num_threads', '-omp',
-                        type=int,
-                        default=12,
-                        help="Number of threads to use for OpenMP")
-    parser.add_argument('--no_single_model', '-nsm',
-                        action='store_true',
-                        help='Set this if every connection should instantiate its own model. Only relevant for custom model, passed using -trt or -fw.')
-    parser.add_argument('--rabbitmq_host', '-rh',
-                        help="RabbitMQ host name")
-    parser.add_argument('--rabbitmq_port', '-rp',
-                        type=int,
-                        help="RabbitMQ port")
-    args = parser.parse_args()
-
-    if args.backend == "tensorrt":
-        if args.trt_model_path is None:
-            raise ValueError("Please Provide a valid tensorrt model path")
-
-    if "OMP_NUM_THREADS" not in os.environ:
-        os.environ["OMP_NUM_THREADS"] = str(args.omp_num_threads)
-
-    if "NSM_FLAG" in os.environ:
-        if os.environ["NSM_FLAG"] == "-nsm":
-            args.no_single_model = True
-
-    download_model(
-        'large-v2',
-        local_files_only=False,
-        cache_dir=None,
-    )
-
-    # if "LD_LIBRARY_PATH" not in os.environ:
-    #     import nvidia.cublas.lib
-    #     import nvidia.cudnn.lib
-    #     print(os.path.dirname(nvidia.cublas.lib.__file__) + ":" + os.path.dirname(nvidia.cudnn.lib.__file__))
-    #     os.environ["LD_LIBRARY_PATH"] = os.path.dirname(nvidia.cublas.lib.__file__) + ":" + os.path.dirname(nvidia.cudnn.lib.__file__)
-    # else:
-    #     print("LD_LIBRARY_PATH already set. Skipping.", os.environ["LD_LIBRARY_PATH"])
-
-    from whisper_live.server import TranscriptionServer
-
-    if args.rabbitmq_host:
-        print(args.rabbitmq_port)
-        rabbitmq_consumer = RabbitMQConsumer(args.rabbitmq_host, args.rabbitmq_port, whisper_port=args.port).run()
-
-    server = TranscriptionServer(single_model=not args.no_single_model)
-    server.run(
-        "0.0.0.0",
-        port=args.port,
-        backend=args.backend,
-        faster_whisper_custom_model_path=args.faster_whisper_custom_model_path,
-        whisper_tensorrt_path=args.trt_model_path,
-        trt_multilingual=args.trt_multilingual,
-        single_model=not args.no_single_model,
-    )
-
-    if args.rabbitmq_host:
-        rabbitmq_consumer.stop()
+import argparseimport osfrom faster_whisper import download_modelfrom whisper_live.support_rabbitmq import RabbitMQConsumerif __name__ == "__main__":    parser = argparse.ArgumentParser()    parser.add_argument('--port', '-p',                        type=int,                        default=9090,                        help="Websocket port to run the server on.")    parser.add_argument('--backend', '-b',                        type=str,                        default='faster_whisper',                        help='Backends from ["tensorrt", "faster_whisper"]')    parser.add_argument('--faster_whisper_custom_model_path', '-fw',                        type=str, default=None,                        help="Custom Faster Whisper Model")    parser.add_argument('--trt_model_path', '-trt',                        type=str,                        default=None,                        help='Whisper TensorRT model path')    parser.add_argument('--trt_multilingual', '-m',                        action="store_true",                        help='Boolean only for TensorRT model. True if multilingual.')    parser.add_argument('--omp_num_threads', '-omp',                        type=int,                        default=12,                        help="Number of threads to use for OpenMP")    parser.add_argument('--no_single_model', '-nsm',                        action='store_true',                        help='Set this if every connection should instantiate its own model. Only relevant for custom model, passed using -trt or -fw.')    parser.add_argument('--rabbitmq_host', '-rh',                        help="RabbitMQ host name")    parser.add_argument('--rabbitmq_port', '-rp',                        type=int,                        help="RabbitMQ port")    args = parser.parse_args()    if args.backend == "tensorrt":        if args.trt_model_path is None:            raise ValueError("Please Provide a valid tensorrt model path")    if "OMP_NUM_THREADS" not in os.environ:        os.environ["OMP_NUM_THREADS"] = str(args.omp_num_threads)    if "NSM_FLAG" in os.environ:        if os.environ["NSM_FLAG"] == "-nsm":            args.no_single_model = True    download_model(        'large-v2',        local_files_only=False,        cache_dir=None,    )    # if "LD_LIBRARY_PATH" not in os.environ:    #     import nvidia.cublas.lib    #     import nvidia.cudnn.lib    #     print(os.path.dirname(nvidia.cublas.lib.__file__) + ":" + os.path.dirname(nvidia.cudnn.lib.__file__))    #     os.environ["LD_LIBRARY_PATH"] = os.path.dirname(nvidia.cublas.lib.__file__) + ":" + os.path.dirname(nvidia.cudnn.lib.__file__)    # else:    #     print("LD_LIBRARY_PATH already set. Skipping.", os.environ["LD_LIBRARY_PATH"])    from whisper_live.server import TranscriptionServer    if args.rabbitmq_host:        print(args.rabbitmq_port)        rabbitmq_consumer = RabbitMQConsumer(args.rabbitmq_host, args.rabbitmq_port, whisper_port=args.port).run()    server = TranscriptionServer(single_model=not args.no_single_model)    server.run(        "0.0.0.0",        port=args.port,        backend=args.backend,        faster_whisper_custom_model_path=args.faster_whisper_custom_model_path,        whisper_tensorrt_path=args.trt_model_path,        trt_multilingual=args.trt_multilingual,        single_model=not args.no_single_model,    )    if args.rabbitmq_host:        rabbitmq_consumer.stop()
